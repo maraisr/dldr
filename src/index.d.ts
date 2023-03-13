756b4dd7c021f87b047d7286eaedad5245bdc9cb
Marais Rossouw <me@marais.co>
2023-03-13 18:02:00 +1000

    fix: loaders can also return an error if they wish

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,3 @@
-export type LoadFn<T> = (keys: string[]) => Promise<T[]>;
+export type LoadFn<T> = (keys: string[]) => Promise<(T | Error)[]>;
 
 export function load<T>(loadFn: LoadFn<T>, key: string): Promise<T>;
