d240e32d87cf16bbe4911c49a13181e65f1e9c05
Marais Rossouw <me@marais.co>
2023-03-13 18:58:11 +1000

    fix: cache instance per loader default

--- src/cache.spec.ts
+++ src/cache.spec.ts
@@ -3,7 +3,24 @@ import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 import * as dldr from './cache';
 
-test('should work', async () => {
+test('should work with default cache', async () => {
+	const loader = spy((keys: string[]) => Promise.resolve(keys));
+
+	// note for tests, we do pollute the global cache
+
+	const items = await Promise.all([
+		dldr.load(loader, undefined, 'a'),
+		dldr.load(loader, undefined, 'b'),
+		dldr.load(loader, undefined, 'c'),
+	]);
+
+	assert.equal(loader.callCount, 1);
+	assert.equal(items[0], 'a');
+	assert.equal(items[1], 'b');
+	assert.equal(items[2], 'c');
+});
+
+test('should work for passed tests', async () => {
 	const loader = spy((keys: string[]) => Promise.resolve(keys));
 
 	const cache = new Map();
@@ -70,6 +87,29 @@ test('cache result should be referentially equal', async () => {
 	assert.equal(a, { key: 'a' });
 	assert.is(a, b);
 	assert.equal(cache.size, 1);
+});
+
+test('should use different cache between loaders (default)', async () => {
+	const loaderA = spy(async (keys: string[]) => keys);
+	const loaderB = spy(async (keys: string[]) => keys);
+
+	let item = await dldr.load(loaderA, undefined, 'a');
+
+	assert.equal(loaderA.callCount, 1);
+	assert.equal(loaderB.callCount, 0);
+	assert.equal(item, 'a');
+
+	item = await dldr.load(loaderA, undefined, 'a');
+
+	assert.equal(loaderA.callCount, 1); // cached
+	assert.equal(loaderB.callCount, 0);
+	assert.equal(item, 'a');
+
+	item = await dldr.load(loaderB, undefined, 'a');
+
+	assert.equal(loaderA.callCount, 1);
+	assert.equal(loaderB.callCount, 1); // different loader
+	assert.equal(item, 'a');
 });
 
 test.run();
--- src/cache.ts
+++ src/cache.ts
@@ -1,13 +1,14 @@
 import { load as _load, type LoadFn } from 'dldr';
 import type { MapLike } from 'dldr/cache';
 
-const container = new Map();
+const container = new WeakMap<LoadFn<any>, Map<string, Promise<any>>>();
 export function load<T>(
 	loadFn: LoadFn<T>,
 	cache: MapLike<string, Promise<T>> | undefined,
 	key: string,
 ): Promise<T> {
-	cache ||= container;
+	cache ||= container.get(loadFn);
+	if (!cache) container.set(loadFn, (cache = new Map()));
 	if (cache.has(key)) return Promise.resolve(cache.get(key)!);
 	const prom = _load(loadFn, key);
 	cache.set(key, prom);
