0c77c344cbb1ac25e16e1b971198698e264c7145
Chanhee Lee <contact@imch.dev>
2023-05-23 08:32:30 +0900

    fix: pass `K` type argument to `LoadFn` (#1)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,6 @@
 export type LoadFn<T, K = string> = (keys: K[]) => Promise<(T | Error)[]>;
 
-export function load<T, K = string>(loadFn: LoadFn<T>, key: K): Promise<T>;
+export function load<T, K = string>(loadFn: LoadFn<T, K>, key: K): Promise<T>;
 
 export function factory<T, K = string>(
 	loadFn: LoadFn<T, K>,
