bbc3d461376dbc7b3fc4ad7dcbb00a2be894f0aa
Marais Rossouw <me@marais.co>
2023-10-19 11:19:44 +1000

    chore: import types as namespace

--- src/cache.ts
+++ src/cache.ts
@@ -1,10 +1,10 @@
-import { load as _load, type LoadFn } from 'dldr';
+import * as dldr from 'dldr';
 import type { MapLike } from 'dldr/cache';
 import { identify } from 'object-identity';
 
-const container = new WeakMap<LoadFn<any, any>, Map<string, Promise<any>>>();
+const container = new WeakMap<dldr.LoadFn<any, any>, Map<string, Promise<any>>>();
 export function load<T, K = string>(
-	loadFn: LoadFn<T, K>,
+	loadFn: dldr.LoadFn<T, K>,
 	cache: MapLike<string, Promise<T>> | undefined,
 	key: K,
 	identity = identify(key),
@@ -12,17 +12,13 @@ export function load<T, K = string>(
 	cache ||= container.get(loadFn);
 	if (!cache) container.set(loadFn, (cache = new Map()));
 	if (cache.has(identity)) return Promise.resolve(cache.get(identity)!);
-	// @ts-expect-error
-	const prom = _load(loadFn, key, identity);
+	const prom = dldr.load(loadFn, key, identity);
 	cache.set(identity, prom);
 	prom.catch(() => cache!.delete(identity));
 	return prom;
 }
 
-export function factory<T, K = string>(
-	loadFn: LoadFn<T, K>,
-	cache?: MapLike<string, Promise<T>> | undefined,
-): (key: K, identity?: string | undefined) => Promise<T> {
+export function factory<T, K = string>(loadFn: dldr.LoadFn<T, K>, cache?: MapLike<string, Promise<T>> | undefined) {
 	return function (key: K, identity?: string | undefined) {
 		return load(loadFn, cache, key, identity);
 	};
